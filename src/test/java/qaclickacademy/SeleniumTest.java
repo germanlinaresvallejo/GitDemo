1972a69050a0c7b1c2621b67bfffd212b89db862
glinares <glinaresvallejo@gmail.com>
2020-03-12 12:51:39 +0100

    American development team

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -20,5 +20,10 @@ public class SeleniumTest {
 	public void elementsU1i(){
 		System.out.println("elementsUi");
 	}
+	
+	@Test
+	public void elementsU1ssi(){
+		System.out.println("elementsUi");
+	}
 
 }
